e3aa4a19ec927842ce03ee8fe0888062961d4d96
Bombsztyl <kacpergumieniuk@gmail.com>
2020-10-12 19:08:01 +0200

    Attacking System

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 let Charmander = {
     name: "CHARMANDER",
     health: 39,
+    maxHealth: 39,
     level: 5
     }
 
 let Squirtle = {
     name: "SQUIRTLE",
     health: 42,
+    maxHealth: 42,
     level: 5
 }    
+
+
+
+const attack = () => {
+    Squirtle.health = Squirtle.health  - 4;
+    printOnScreen();
+}
 
 
     const printOnScreen = () => {
        
         document.getElementById("player-name").innerText = Charmander.name;
-        document.getElementById("player-hp-current").innerText = Charmander.health;
-        document.getElementById("player-hp-max").innerText = Charmander.health;
+         document.getElementById("player-hp-current").innerText = Charmander.health;
+        document.getElementById("player-hp-max").innerText = Charmander.maxHealth;
         document.getElementById("player-level").innerText = "L" + Charmander.level;
 
         document.getElementById("enemy-name").innerText = Squirtle.name;
-        document.getElementById("enemy-hp-current").innerText = Squirtle.health;
-        document.getElementById("enemy-hp-max").innerText = Squirtle.health;
+         document.getElementById("enemy-hp-current").innerText = Squirtle.health;
+        document.getElementById("enemy-hp-max").innerText = Squirtle.maxHealth;
         document.getElementById("enemy-level").innerText = "L" + Squirtle.level;
 
     }
 
+     
     printOnScreen();
