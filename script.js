4d522a2bd1060b4a523e05da74f9dd1ab4e32fc5
Tommy Tran <tommyktran@gmail.com>
2020-10-12 14:09:45 -0400

    Add gameMessage() function

--- script.js
+++ script.js
@@ -22,19 +22,25 @@ const attack = () => {
 }
 
 
-    const printOnScreen = () => {
-       
-        document.getElementById("player-name").innerText = Charmander.name;
-         document.getElementById("player-hp-current").innerText = Charmander.health;
-        document.getElementById("player-hp-max").innerText = Charmander.maxHealth;
-        document.getElementById("player-level").innerText = "L" + Charmander.level;
+const printOnScreen = () => {
+    
+    document.getElementById("player-name").innerText = Charmander.name;
+    document.getElementById("player-hp-current").innerText = Charmander.health;
+    document.getElementById("player-hp-max").innerText = Charmander.maxHealth;
+    document.getElementById("player-level").innerText = "L" + Charmander.level;
 
-        document.getElementById("enemy-name").innerText = Squirtle.name;
-         document.getElementById("enemy-hp-current").innerText = Squirtle.health;
-        document.getElementById("enemy-hp-max").innerText = Squirtle.maxHealth;
-        document.getElementById("enemy-level").innerText = "L" + Squirtle.level;
+    document.getElementById("enemy-name").innerText = Squirtle.name;
+    document.getElementById("enemy-hp-current").innerText = Squirtle.health;
+    document.getElementById("enemy-hp-max").innerText = Squirtle.maxHealth;
+    document.getElementById("enemy-level").innerText = "L" + Squirtle.level;
 
-    }
+}
 
-     
-    printOnScreen();
+function gameMessage(message) {
+    document.getElementById("game-menu").style = "display: none;";
+    document.getElementById("message").style = "display: block;";
+    document.getElementById("message").innerHTML = message;
+}
+    
+printOnScreen();
+// gameMessage("CHARMANDER used SCRATCH!");
